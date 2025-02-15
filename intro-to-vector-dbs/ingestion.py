fc996a7e42fbd98829443179afdcb23a48fa604b
Hiroto0706 <mirunina2366@icloud.com>
2025-02-15 12:44:01 +0900

    add: embedding to vector db

--- intro-to-vector-dbs/ingestion.py
+++ intro-to-vector-dbs/ingestion.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
@@ -9,3 +10,19 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Ingesting...")
+
+    loader = TextLoader(os.getenv("PATH_TO_DOC"))
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    print("Ingesting...")
+    PineconeVectorStore.from_documents(
+        texts, embeddings, index_name=os.getenv("INDEX_NAME")
+    )
+    print("Finished!!")
