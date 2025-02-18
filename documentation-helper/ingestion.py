57645c9ab4b7261f354c778a21d38b2b466b4182
Hiroto Kadota <hiroto.kadota@anymindgroup.com>
2025-02-18 21:03:15 +0900

    [add] added ingestion system and summary chunk size

--- documentation-helper/ingestion.py
+++ documentation-helper/ingestion.py
@@ -1,4 +1,5 @@
-from langchain_pinecone import PineconeVectorStore
+import os
+from langchain_pinecone import Pinecone, PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -21,6 +22,18 @@ def ingest_docs():
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=600, chunk_overlap=50
     )
+    documents = text_splitter.split_documents(raw_documents)
+
+    for doc in documents:
+        new_url = doc.metadata["source"]
+        new_url = new_url.replace("langchain-docs", "http:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to add {len(documents)} to Pinecone")
+    PineconeVectorStore.from_documents(
+        documents, embeddings, index_name="langchain-doc-index"
+    )
+    print("****Loading to vectorstore done ****")
 
 
 if __name__ == "__main__":
