7b928608e12674312ed8702b1ddf6d25a31a7439
Hiroto Kadota <hiroto.kadota@anymindgroup.com>
2025-02-19 21:25:07 +0900

    [add] added ritch ui chat app

---
+++ documentation-helper/main.py
@@ -0,0 +1,54 @@
+from typing import Set
+from backend.core import run_llm
+import streamlit as st
+
+
+st.header("LangChain Udemy Course- Documentation Helper Bot")
+st.subheader("Main Page")
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here...")
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
+
+def created_source_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list, 1):
+        sources_string += f"{i}. {source}\n"
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating response..."):
+        generated_response = run_llm(query=prompt)
+        sources = set([
+            doc.metadata["source"]
+            for doc in generated_response["source_documents"]
+        ])
+
+        formatted_response = f"{generated_response['result']} \n\n {created_source_string(sources)}"
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for user_query, generatted_response in zip(st.session_state["user_prompt_history"], st.session_state["chat_answers_history"]):
+        st.chat_message("user").write(user_query)
+        st.chat_message("assistant").write(generatted_response)
+
+custom_css = """
+<style>
+.stChatMessage {
+    overflow-wrap: break-word;
+}
+</style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
