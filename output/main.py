c3c4c4b2fa571ad959d825a75e4e4ca78525970f
Hiroto0706 <mirunina2366@icloud.com>
2025-02-26 11:25:49 +0900

    [add] add notion document loader

--- output/main.py
+++ output/main.py
@@ -4,7 +4,7 @@ from rag import create_rag_chain
 from split import split_text
 from langchain_core.output_parsers import StrOutputParser
 
-from text_load import loading_text
+from text_load import loading_notion_db, loading_text
 
 
 load_dotenv()
@@ -13,25 +13,27 @@ load_dotenv()
 def main(username: str = "test"):
     print("Start...!")
 
-    documents = loading_text("note_1.txt")
+    # documents = loading_text("note_1.txt")
+    documents = loading_notion_db()
+    print(f"documents {documents}")
 
     docs = split_text(documents, 2000, 200)
     print(f"text length {len(docs)}")
 
-    embedding_document(username, docs)
+    # embedding_document(username, docs)
 
-    vectorstore = create_vectorstore(username)
-    chain = create_rag_chain(retriever=vectorstore.as_retriever())
+    # vectorstore = create_vectorstore(username)
+    # chain = create_rag_chain(retriever=vectorstore.as_retriever())
 
-    print(
-        chain.invoke(
-            {
-                "input": "行動経済学の理解度をチェックするための確認問題を10問生成してください。それぞれの問題は4択の選択形式の問題であること。最終的なアウトプットはJSON形式でquestion, options, answer, explanationの4つで構成されること。問題の難易度は難しいでお願いします"
-            }
-        )["answer"]
-    )
+    # print(
+    #     chain.invoke(
+    #         {
+    #             "input": "行動経済学の理解度をチェックするための確認問題を10問生成してください。それぞれの問題は4択の選択形式の問題であること。最終的なアウトプットはJSON形式でquestion, options, answer, explanationの4つで構成されること。問題の難易度は難しいでお願いします"
+    #         }
+    #     )["answer"]
+    # )
 
-    clean_up_vectorstore(username)
+    # clean_up_vectorstore(username)
 
 
 if __name__ == "__main__":
--- output/text_load.py
+++ output/text_load.py
@@ -1,8 +1,21 @@
+import os
 from typing import List
 from langchain_core.documents import Document
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, NotionDBLoader
+
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+DATABASE_ID = os.getenv("DATABASE_ID")
 
 
 def loading_text(target_file: str) -> List[Document]:
     loader = TextLoader("./assets/" + target_file)
+    return loader.load()
+
+
+def loading_notion_db() -> List[Document]:
+    loader = NotionDBLoader(
+        integration_token=NOTION_TOKEN,
+        database_id=DATABASE_ID,
+        request_timeout_sec=30
+    )
     return loader.load()
